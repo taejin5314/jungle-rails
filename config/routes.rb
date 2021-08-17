4bb594d1843cd71e4d17811e2d556396f94583bf
Taejin Jung <taejin5314@gmail.com>
2021-08-17 23:14:23 +0000

    add login signup page and implement authentication

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :create, :new]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
